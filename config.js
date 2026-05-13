e46cd37602dfa9c277b2efddd59c2ab86168e08f
kennetopend-a11y <kennetopend@gmail.com>
2026-05-13 05:25:18 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+// config.js
+module.exports = {
+    // Your exact phone number structure with country code mapped into the system database
+    ownerNumber: "254727844623@s.whatsapp.net", 
+    botName: "Haimiya-senpai",
+    prefix: ".",
+    currencySymbol: "¥",
+    dailyPayout: 1083
+};
